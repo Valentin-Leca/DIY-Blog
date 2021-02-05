400e5d6051d00f422576ee79861e39911845b5a5
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2021-02-05 17:31:19 +0100

    add comment table in database

---
+++ src/Entity/ArticleCategory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleCategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleCategoryRepository::class)
+ */
+class ArticleCategory
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $article_category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArticleCategory(): ?string
+    {
+        return $this->article_category;
+    }
+
+    public function setArticleCategory(string $article_category): self
+    {
+        $this->article_category = $article_category;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Comment.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment_content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $comment_author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $comment_date;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $comment_report;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $comment_article_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentContent(): ?string
+    {
+        return $this->comment_content;
+    }
+
+    public function setCommentContent(string $comment_content): self
+    {
+        $this->comment_content = $comment_content;
+
+        return $this;
+    }
+
+    public function getCommentAuthor(): ?string
+    {
+        return $this->comment_author;
+    }
+
+    public function setCommentAuthor(string $comment_author): self
+    {
+        $this->comment_author = $comment_author;
+
+        return $this;
+    }
+
+    public function getCommentDate(): ?\DateTimeInterface
+    {
+        return $this->comment_date;
+    }
+
+    public function setCommentDate(\DateTimeInterface $comment_date): self
+    {
+        $this->comment_date = $comment_date;
+
+        return $this;
+    }
+
+    public function getCommentReport(): ?int
+    {
+        return $this->comment_report;
+    }
+
+    public function setCommentReport(?int $comment_report): self
+    {
+        $this->comment_report = $comment_report;
+
+        return $this;
+    }
+
+    public function getCommentArticleId(): ?int
+    {
+        return $this->comment_article_id;
+    }
+
+    public function setCommentArticleId(int $comment_article_id): self
+    {
+        $this->comment_article_id = $comment_article_id;
+
+        return $this;
+    }
+}
