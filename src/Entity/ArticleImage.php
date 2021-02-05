80b5dee7b56fd23e4238289054999c891542c31d
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2021-02-05 17:34:51 +0100

    add article image table in database

---
+++ src/Entity/ArticleImage.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleImageRepository::class)
+ */
+class ArticleImage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->image_name;
+    }
+
+    public function setImageName(string $image_name): self
+    {
+        $this->image_name = $image_name;
+
+        return $this;
+    }
+}
