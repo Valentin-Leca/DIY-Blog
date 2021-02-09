edd4141826b918fb898df95ab8978a4f34936596
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2021-02-09 17:01:09 +0100

    create crud for Article table with make:crud command

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('article_name')
+            ->add('article_content')
+            ->add('article_author')
+            ->add('article_date')
+            ->add('article_like')
+            ->add('articleImage')
+            ->add('articleCategory')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
